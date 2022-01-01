5dac3ae2dba3a8d73cad7e107b6a4d4ebc45c883
nguyenvukhang <brew4k@gmail.com>
2022-01-01 22:45:14 +0800

    made every mdx file wrapped by the same container

---
+++ pages/[slug].js
@@ -0,0 +1,48 @@
+import fs from 'fs'
+import path from 'path'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import MDXComponents from '@/components/MDXComponents'
+import Container from '@/components/Container'
+
+const root = process.cwd()
+
+export default function Page({ source }) {
+  return (
+    <Container>
+      <MDXRemote {...source} components={MDXComponents} />
+    </Container>
+  )
+}
+
+async function getFiles(type) {
+  return fs.readdirSync(path.join(root, type))
+}
+
+// this is ran before getStaticProps
+export async function getStaticPaths() {
+  const files = await getFiles('content')
+  return {
+    fallback: false,
+    paths: files.map((f) => ({
+      params: {
+        slug: f.replace(/\.mdx/, ''),
+      },
+    })),
+  }
+}
+
+async function getFileBySlug(slug) {
+  const raw = fs.readFileSync(
+    path.join(root, 'content', `${slug}.mdx`),
+    'utf-8'
+  )
+  const source = await serialize(raw)
+  return { source }
+}
+
+// this is ran after getStaticPaths
+export async function getStaticProps({ params }) {
+  const content = await getFileBySlug(params.slug)
+  return { props: { ...content } }
+}
