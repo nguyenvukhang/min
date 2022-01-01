a9b292583fa238d3147c2c113acac3dda154b520
nguyenvukhang <brew4k@gmail.com>
2022-01-01 22:02:32 +0800

    injected mdx custom components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '@/styles/global.css'
+import { MDXProvider } from '@mdx-js/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const H1 = ({ children }) => {
+    return <h1 className="text-3xl">{children}</h1>
+  }
+  const UL = ({ children }) => {
+    return <h1 className="text-3xl">{children}</h1>
+  }
+  const components = {
+    h1: H1,
+    ul: UL,
+  }
+
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
 }
 
 export default MyApp
