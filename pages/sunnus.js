84a374c6bd7a119fade54b4327ed90df871f7b4c
nguyenvukhang <brew4k@gmail.com>
2022-01-07 08:47:50 +0800

    patched chakra

--- pages/sunnus.js
+++ pages/sunnus.js
@@ -5,6 +5,17 @@ import { MDXRemote } from 'next-mdx-remote'
 import Container from '@/components/Container'
 import sunNUSComponents from '@/components/sunNUSComponents'
 import { ChakraProvider } from '@chakra-ui/provider'
+import { extendTheme } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/color-mode";
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 const root = process.cwd()
 const mdxFilename = 'exco-meet-1'
@@ -12,6 +23,7 @@ const mdxFilename = 'exco-meet-1'
 export default function Page({ source }) {
   return (
     <Container>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider>
         <MDXRemote {...source} components={sunNUSComponents} />
       </ChakraProvider>
