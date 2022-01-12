ebde24b21767ae8706d373b3167129eb6c8fd66a
nguyenvukhang <brew4k@gmail.com>
2022-01-12 11:16:14 +0800

    fixed crossorigin

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="en" className="bg-brew-light dark:bg-brew-dark">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
             rel="stylesheet"
